871bf3fd1a3e9491be565038c99a89e545fc6106
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-09-17 09:29:59 +0200

    points

---
+++ swingy/src/main/java/ktrout/util/mapPoints.java
@@ -0,0 +1,28 @@
+package ktrout.util;
+
+public class mapPoints {
+	
+	private int x;
+	private int y;
+	
+	public mapPoints(int x, int y) {
+		x = this.x;
+		y = this.y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		x = this.x;
+	}
+	
+	public void setY(int y) {
+		y = this.y;
+	}
+}
